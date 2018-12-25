acca3ad3d065f65954d371839f8a132e86617aa4
James Woolfenden <james.woolfenden@gmail.com>
2018-12-25 19:42:34 +0000

    switch example to use registry

--- examples/exampleA/module.s3.tf
+++ examples/exampleA/module.s3.tf
@@ -1,5 +1,5 @@
 module "s3" {
-  source                  = "jameswoolfenden/aws/s3"
+  source                  = "JamesWoolfenden/s3/aws"
   version                 = "0.0.3"
   common_tags             = "${var.common_tags}"
   s3_bucket_force_destroy = "${var.s3_bucket_force_destroy}"
