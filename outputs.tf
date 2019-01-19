3de98d10edc5eae8d95341eee4e81610dd9c434a
James Woolfenden <James.Woolfenden@gmail.com>
2019-01-19 17:53:43 +0000

    New readme

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "s3_id" {
-  value = "${aws_s3_bucket.bucket.id}"
+  value       = "${aws_s3_bucket.bucket.id}"
+  description = "The id of the bucket"
 }
 
 output "bucket_domain_name" {
-  value = "${aws_s3_bucket.bucket.bucket_domain_name}"
+  value       = "${aws_s3_bucket.bucket.bucket_domain_name}"
+  description = "The full domain name of the bucket"
 }
 
 output "account_id" {
-  value = "${data.aws_caller_identity.current.account_id}"
+  value       = "${data.aws_caller_identity.current.account_id}"
+  description = "The AWS account number in use"
 }
