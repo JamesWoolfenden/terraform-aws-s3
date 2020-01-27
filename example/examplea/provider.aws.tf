b0c7317af7b8f07c00005fcaa9268706cc25951d
James Woolfenden <james.woolfenden@slalom.com>
2020-01-27 21:20:35 +0000

    clean up

---
+++ example/examplea/provider.aws.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region  = "eu-west-1"
+  version = "2.41.0"
+}
+
+provider "template" {
+  version = "2.1"
+}
---
+++ example/examplea/variables.tf
@@ -0,0 +1,14 @@
+variable "common_tags" {
+  description = "This is a map type for applying tags on resources"
+  type        = map
+}
+
+variable "s3_bucket_name" {
+  description = "The name of the bucket"
+  type        = string
+}
+
+variable "s3_bucket_force_destroy" {
+  description = "String Boolean to set bucket to be undeletable (well more difficult anyway)"
+  type        = string
+}
