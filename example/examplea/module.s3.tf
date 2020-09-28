7c5a10b6db7e448d0c4f474c3c7b891a2d902df5
James Woolfenden <James.Woolfenden@gmail.com>
2020-09-29 00:35:28 +0100

    fix

--- example/examplea/module.s3.tf
+++ example/examplea/module.s3.tf
@@ -3,6 +3,6 @@ module "s3" {
   common_tags             = var.common_tags
   s3_bucket_force_destroy = var.s3_bucket_force_destroy
   s3_bucket_name          = "${var.s3_bucket_name}${data.aws_caller_identity.current.account_id}"
-  s3_bucket_policy        = data.aws_iam_policy_document.s3_policy.json
+  s3_bucket_policy        = ""
   mfa_delete              = var.mfa_delete
 }
