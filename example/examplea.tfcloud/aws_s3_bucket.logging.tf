2fd3261f7ef55f5139a9a4f7bf37264ad290c4e0
James Woolfenden <James.Woolfenden@gmail.com>
2021-03-31 10:48:35 +0100

    refresh

--- example/examplea.tfcloud/aws_s3_bucket.logging.tf
+++ example/examplea.tfcloud/aws_s3_bucket.logging.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "logging" {
-  #checkov:skip=CKV_AWS_18:This is a logging bucket
-  #checkov:skip=CKV_AWS_21:Versioning for logs? Yeah no
+  # checkov:skip=CKV_AWS_18:This is a logging bucket
+  # checkov:skip=CKV_AWS_21:Versioning for logs? Yeah no
+  # checkov:skip=CKV_AWS_52:MFA requirement breaks tf
   acl    = "log-delivery-write"
   bucket = "logging-${data.aws_caller_identity.current.account_id}"
   server_side_encryption_configuration {
