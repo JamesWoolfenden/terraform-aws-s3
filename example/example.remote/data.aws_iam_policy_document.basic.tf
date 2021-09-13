e7e4aa5cc8334f89b6283ac1773c52d77ce58a3a
James Woolfenden <James.Woolfenden@gmail.com>
2021-09-13 09:12:32 +0100

    crossa account

---
+++ example/example.remote/data.aws_iam_policy_document.basic.tf
@@ -0,0 +1,42 @@
+data "aws_iam_policy_document" "basic" {
+
+  # statement {
+  #   sid    = "AllowPublicReadGetObject"
+  #   effect = "Allow"
+  #   principals {
+  #     type        = "*"
+  #     identifiers = ["*"]
+  #   }
+  #   actions   = ["s3:GetObject"]
+  #   resources = ["arn:aws:s3:::${local.full_s3_bucket_name}/*"]
+  # }
+
+  # statement {
+  #   sid    = "DenyHttp"
+  #   effect = "Deny"
+  #   principals {
+  #     type        = "*"
+  #     identifiers = ["*"]
+  #   }
+  #   actions   = ["s3:GetObject"]
+  #   resources = ["arn:aws:s3:::${local.full_s3_bucket_name}/*"]
+  #   condition {
+  #     test     = "StringEquals"
+  #     values   = ["false"]
+  #     variable = "aws:SecureTransport"
+  #   }
+  # }
+
+  statement {
+    sid    = "Example permissions bridgecrew"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::890234264427:root"]
+    }
+    actions = ["s3:GetBucketLogging",
+    "s3:GetBucketAcl"]
+    resources = ["arn:aws:s3:::${local.full_s3_bucket_name}"]
+  }
+
+}
